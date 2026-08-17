0e94b80f8c226c533aca48bd10b3f9d1fc3ac7a3
MD ALAM <alkaserphone@MacBook-Air.local>
2026-08-17 17:37:07 +0545

    add examples of union types and type assignments in TypeScript

---
+++  01-basics/src/typesInTs.ts
@@ -0,0 +1,7 @@
+let day: string = "monday";
+day = "tuesday";
+
+let bookname: string = "the great gastby";
+bookname = "alice in wonderland";
+
+// bookname=30 // error: Type 'number' is not assignable to type 'string'.
